43fa6f45c6c1525d16d41c2e0d54eded8d2787e8
Marcia Satie <111194629+MarciaSatie@users.noreply.github.com>
2025-07-19 22:03:23 +0100

    adding login

--- routes.js
+++ routes.js
@@ -3,16 +3,31 @@ import { dashboardController } from "./controllers/dashboard-controller.js";
 import { aboutController } from "./controllers/about-controller.js";
 import { weatherTopController } from "./controllers/weatherTop-controller.js";
 import { stationController } from "./controllers/station-controller.js";
+import { accountsController } from './controllers/accounts-controller.js';
 
+console.log("dashboardController import:", dashboardController);
 
 export const router = express.Router();
 
-router.get("/", dashboardController.index);
+// Accounts routes
+router.get("/", accountsController.index);
+router.get("/login", accountsController.login);
+router.get("/signup", accountsController.signup);
+router.get("/logout", accountsController.logout);
+router.post("/register", accountsController.register);
+router.post("/authenticate", accountsController.authenticate);
+
+//App routes
 router.get("/dashboard", dashboardController.index);
 router.get("/about", aboutController.index);
 router.get("/weatherTop", weatherTopController.index);
 router.post("/dashboard/addStation", dashboardController.addStation);
 router.get("/station/:id", stationController.index);
 router.post("/station/:id/addReport", stationController.addReport);
+//router.get("/station/:id/delete", stationController.deleteStation);
+
+
+
+
 
 
